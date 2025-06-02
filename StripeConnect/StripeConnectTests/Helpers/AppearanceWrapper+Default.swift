91c4986926119c25ea1d4e80037ef3ee804cda78
David Estes <davidestes@stripe.com>
2025-06-02 13:02:41 -0700

    Stripe SDK 24.15.0

---
+++ StripeConnect/StripeConnectTests/Helpers/AppearanceWrapper+Default.swift
@@ -0,0 +1,13 @@
+//
+//  AppearanceWrapper+Default.swift
+//  StripeConnectTests
+//
+//  Created by Chris Mays on 9/6/24.
+//
+
+import Foundation
+@testable import StripeConnect
+
+extension AppearanceWrapper {
+    public static let `default`: AppearanceWrapper = .init(appearance: .default, traitCollection: .init())
+}
